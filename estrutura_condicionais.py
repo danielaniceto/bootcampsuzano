1eb3b3fc82c514ceb2604eafb86925deee5660cf
danielaniceto <danielgustavots@hotmail.com>
2025-03-04 03:35:03 -0300

    Primeiro Commit

---
+++ estrutura_condicionais.py
@@ -0,0 +1,15 @@
+MAIOR_IDADE = 18
+
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print("Autorizado a tirar a habilitação!!!")
+
+if idade < 18:
+    print("Não autorizado a tirar a habilitação!!!")
+
+if idade >= 18:
+    print("Autorizado a tirar a habilitação!!!")
+
+else:
+    print("Não autorizado a tirar a habilitação!!!")
