bd6a37e41b2474809e849ffb84afee8eec9c70be
danielaniceto <danielgustavots@hotmail.com>
2025-03-07 02:23:31 -0300

    Strings

---
+++ string_1.py
@@ -0,0 +1,19 @@
+nome = "dAnIel"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = "     texto       de     exemplo        "
+
+print(texto + ".")
+print(texto.strip() + ".")
+print(texto.lstrip() + ".")
+print(texto.rstrip() + ".")
+
+menu = "Daniel"
+
+print("####" + menu + "####")
+print(menu.center(14))
+print(menu.center(14, "#"))
+print("-".join(menu))
