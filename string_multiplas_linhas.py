54cf5d75b4765db283dc1db156344623e865b944
danielaniceto <danielgustavots@hotmail.com>
2025-03-10 00:32:43 -0300

    Strings multiplas linhas

---
+++ string_multiplas_linhas.py
@@ -0,0 +1,24 @@
+nome = "Daniel"
+
+mensagem = f"""Ola meu nome e {nome}, 
+    eu tenho 30 anos e sou programador em Python,
+            Essa mensagem tem diferentes recuos
+"""
+
+print(mensagem)
+
+print(
+    """
+    ==========  MENU  ==========
+    
+    1 - Dositar
+    2 - Sacar
+    3 - Transferir
+    0 - Sair
+
+    ============================
+
+    Obrigado por usar nosso banco
+    
+    """
+)
